bb988a384cdec1237097073307ef6e2e10362a3b
CeznVi <cezn.vi@gmail.com>
2024-08-05 19:55:21 +0300

    add - npm install @googlemaps/js-api-loader

--- reactgooglemaps/src/main.tsx
+++ reactgooglemaps/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+/// строгий режим отключаем так как возможны баги в некоторых местах
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+        <App />
 )
