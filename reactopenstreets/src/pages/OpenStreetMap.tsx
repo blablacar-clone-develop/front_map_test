3fd2b20192967f48cf46b131ca2a634b0c3f09c5
Sofia <sofapokl@gmail.com>
2024-08-21 18:02:01 +0300

    Open Street Map Dark Theme

--- reactopenstreets/src/pages/OpenStreetMap.tsx
+++ reactopenstreets/src/pages/OpenStreetMap.tsx
@@ -5,7 +5,6 @@ import '../styles/OpenStreetMap.css';
 
 const OpenStreetMap: React.FC = () => {
     useEffect(() => {
-        // Перевірка наявності попередньої ініціалізації карти
         if (L.DomUtil.get('osmMap')?.hasChildNodes()) {
             const mapContainer = L.DomUtil.get('osmMap');
             if (mapContainer) {
@@ -15,24 +14,27 @@ const OpenStreetMap: React.FC = () => {
 
         const map = L.map('osmMap').setView([51.505, -0.09], 13);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        // Темна тема CartoDB Dark Matter
+        L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         }).addTo(map);
 
-        // Кастомний маркер з простим спливаючим вікном
         const marker = L.marker([51.505, -0.09]).addTo(map);
-
         marker.bindPopup(
             `<div style="text-align:center;">
-        <strong>HIIIIII</strong><br>
+        <strong>Dark theme</strong><br>
+       
       </div>`,
             { closeButton: false, minWidth: 150 }
         );
 
-        // Відкрити спливаюче вікно автоматично при завантаженні карти
         marker.openPopup();
 
-        // Очистка карти при розмонтуванні компонента
+        setTimeout(() => {
+            marker.setLatLng([51.51, -0.1]);
+            marker.openPopup();
+        }, 2000);
+
         return () => {
             map.remove();
         };
